7f3373a4dec323cb6756ed56881f28df6b307c88
Nikesh Sitaula <31296289+nikeshsitaula@users.noreply.github.com>
2021-12-09 09:18:21 +1100

    Update connection.php

--- customer/include/connection.php
+++ customer/include/connection.php
@@ -1,9 +1,9 @@
 
 <?php
-$host="localhost";
-$uname='root';
-$pass='';
-$dbname='travel_website';
+$host="us-cdbr-east-04.cleardb.com";
+$uname='b8a7f0a7c058da';
+$pass='b0154a64';
+$dbname='heroku_d955542c77e89d6';
 try {
   $conn = new PDO("mysql:host=$host;dbname=$dbname", $uname, $pass);
   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
